480666c06e6432d96031e8371c0d49911b573bbb
Kevin Lezcano <kevin_lezcano@hotmail.com>
2024-10-31 08:06:50 -0300

    Microservicios corregido

--- microservice-course/src/main/java/com/microservice/course/service/CourseServiceImpl.java
+++ microservice-course/src/main/java/com/microservice/course/service/CourseServiceImpl.java
@@ -5,9 +5,11 @@ import com.microservice.course.dto.StudentDTO;
 import com.microservice.course.entities.Course;
 import com.microservice.course.http.response.StudentByCourseResponse;
 import com.microservice.course.persistence.ICourseRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 @Service
 public class CourseServiceImpl implements ICourseService {
@@ -34,6 +36,7 @@ public class CourseServiceImpl implements ICourseService {
     }
 
     @Override
+    @CircuitBreaker(name = "studentService", fallbackMethod = "fallbackFindStudentsByCourse")
     public StudentByCourseResponse findStudentsByCourse(Long idCourse) {
         Course course = courseRepository.findById(idCourse).orElse(new Course());
         List<StudentDTO> studentDTOList = studentClient.findAllStudentByCourse(idCourse);
@@ -41,6 +44,17 @@ public class CourseServiceImpl implements ICourseService {
                 .courseName(course.getName())
                 .teacher(course.getTeacher())
                 .studentDTOList(studentDTOList)
+                .build();
+    }
+
+
+    public StudentByCourseResponse fallbackFindStudentsByCourse(Long idCourse, Throwable throwable) {
+        System.out.println("Circuit Breaker activate: " + throwable.getMessage());
+        Course course = courseRepository.findById(idCourse).orElse(new Course());
+        return StudentByCourseResponse.builder()
+                .courseName(course.getName())
+                .teacher(course.getTeacher())
+                .studentDTOList(Collections.emptyList()) // Devuelve una lista vacía si falla
                 .build();
     }
 }
